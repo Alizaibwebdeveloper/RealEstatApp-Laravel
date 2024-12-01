8bbaaba2faa8da69c13c61e3f08fab482f9ef83c
Ali zaib <alizaib12220@gmail.com>
2024-12-01 16:09:00 +0500

    Seeder and factory Inserting demouser Data

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+    DB::table('users')->insert([
+     // Admin
+
+
+        [
+            'name'=> 'Admin',
+            'username'=> 'admin',
+            'email'=>'admin@gmail.com',
+            'password'=>Hash::make('123456789'),
+            'role'=>'admin',
+            'status'=> 'active'
+        ],
+
+        // Agent
+        
+        [
+            'name'=> 'Agent',
+            'username'=> 'agent',
+            'email'=>'agent@gmail.com',
+            'password'=>Hash::make('123456789'),
+            'role'=>'agent',
+            'status'=> 'active'
+        ],
+          
+        // User
+        
+        [
+            'name'=> 'User',
+            'username'=> 'user',
+            'email'=>'user@gmail.com',
+            'password'=>Hash::make('123456789'),
+            'role'=>'user',
+            'status'=> 'active'
+        ],
+    ]);
+
+    }
+}
